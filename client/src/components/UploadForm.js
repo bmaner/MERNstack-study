d075807030a8c0fe8543b95168460f5ab259e8d4
seonghyeon park <seonghyeonpark@seonghyeonui-MacBookPro.local>
2021-11-26 09:23:34 +0900

    Add ProgressBar component

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { toast } from 'react-toastify';
+import './UploadForm.css';
+import ProgressBar from './ProgressBar';
 
 function UploadForm() {
+    const defaultFileName = '이미지 파일을 업로드 해주세요.';
     const [file, setFile] = useState(null);
-    const [fileName, setFileName] = useState('이미지 파일을 업로드 해주세요.');
+    const [fileName, setFileName] = useState(defaultFileName);
+    const [percent, setPercent] = useState(0);
 
     function imageSelectHandler(e) {
         const imageFile = e.target.files[0];
@@ -21,24 +26,46 @@ function UploadForm() {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
+                onUploadProgress: e => {
+                    setPercent(Math.round((100 * e.loaded) / e.total));
+                },
             });
-            console.log(res);
-            alert('success!');
+            // console.log(res);
+            toast.success('이미지 업로드 성공!');
+            setTimeout(() => {
+                setPercent(0);
+                setFileName(defaultFileName);
+            }, 3000);
         } catch (err) {
-            alert('fail!');
-            console.error(err);
+            toast.error(err.message);
+            setPercent(0);
+            setFileName(defaultFileName);
+            // console.error(err);
         }
     }
 
     return (
         <form onSubmit={e => onSubmit(e)}>
-            <label htmlFor="image">{fileName}</label>
-            <input
-                id="image"
-                type="file"
-                onChange={e => imageSelectHandler(e)}
-            />
-            <button type="submit">제출</button>
+            <ProgressBar percent={percent} />
+            <div className="file-dropper">
+                {fileName}
+                <input
+                    id="image"
+                    type="file"
+                    onChange={e => imageSelectHandler(e)}
+                />
+            </div>
+            <button
+                type="submit"
+                style={{
+                    width: '100%',
+                    height: '40px',
+                    borderRadius: '3px',
+                    cursor: 'pointer',
+                }}
+            >
+                제출
+            </button>
         </form>
     );
 }
