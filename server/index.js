3aa8a3d36bc6eb3d1444db75414501a0e3591ccc
bmaner <bmanerdaniel@gmail.com>
2021-12-02 23:19:59 +0900

    Add user schema

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 // console.log('uuid', uuid()); // uuid 35c68427-f597-42ae-b932-d4e5d3f2267d
 const mongoose = require('mongoose');
 const { imageRouter } = require('./routes/imageRouter');
+const { userRouter } = require('./routes/UserRouter');
 
 const app = express();
 const { MONGO_URI, PORT } = process.env;
@@ -12,7 +13,9 @@ mongoose
     .then(() => {
         console.log('mongoDB connected!');
         app.use('/uploads', express.static('uploads'));
+        app.use(express.json());
         app.use('/images', imageRouter);
+        app.use('/users', userRouter);
         app.listen(PORT, () =>
             console.log(`Express server listening on PORT ${PORT}`)
         );
