a8ee788048afa894fbb3e43359245842600bc1cd
bmaner <bmanerdaniel@gmail.com>
2021-12-05 10:36:41 +0900

    Add bcryptjs for hashing and login router

--- server/routes/UserRouter.js
+++ server/routes/UserRouter.js
@@ -1,11 +1,38 @@
 const { Router } = require('express');
 const userRouter = Router();
 const User = require('../models/User');
+const { hash, compare } = require('bcryptjs');
 
 userRouter.post('/register', async (req, res) => {
-    console.log(req.body);
-    const user = await new User(req.body).save().catch(err => console.log(err));
-    res.json({ message: 'user registered' });
+    try {
+        if (req.body.password.length < 8) {
+            throw new Error('비밀번를 최소 8자 이상으로 해주세요.');
+        }
+        const hashedPassword = await hash(req.body.password, 10);
+        const user = await new User({
+            name: req.body.name,
+            username: req.body.username,
+            hashedPassword,
+        })
+            .save()
+            .catch(err => console.log(err));
+        res.json({ message: 'user registered' });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+userRouter.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            username: req.body.username,
+        });
+        const isValid = await compare(req.body.password, user.hashedPassword);
+        if (!isValid) throw new Error('입력하신 정보가 올바르지 않습니다.');
+        res.json({ message: 'user validated' });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
 
 module.exports = { userRouter };
