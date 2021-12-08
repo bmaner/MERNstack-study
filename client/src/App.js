d6d7d6a500e66727233d850b800174dd3ed4fd77
bmaner <bmanerdaniel@gmail.com>
2021-12-08 22:09:00 +0900

    Add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-
-import UploadForm from './components/UploadForm';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ImageList from './components/ImageList';
-import { ImageProvider } from './context/ImageContext';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import RegisterPage from './pages/RegisterPage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div style={{ maxWidth: 600, margin: 'auto' }}>
             <ToastContainer />
-            <h2>사진첩</h2>
-            <UploadForm />
-            <ImageList />
+            <Routes>
+                <Route path="/" exact element={<MainPage />} />
+                <Route path="/auth/register" exact element={<RegisterPage />} />
+                <Route path="/auth/login" exact element={<LoginPage />} />
+            </Routes>
         </div>
     );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ImageProvider } from './context/ImageContext';
 
 ReactDOM.render(
     <React.StrictMode>
-        <ImageProvider>
-            <App />
-        </ImageProvider>
+        <BrowserRouter>
+            <ImageProvider>
+                <App />
+            </ImageProvider>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
